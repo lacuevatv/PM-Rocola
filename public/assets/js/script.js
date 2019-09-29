313142e9075bcb903c8e6d7706c37978b624a811
coco <coco@lacueva.tv>
2019-09-29 15:36:56 -0300

    armando la rockola

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,19 @@
 $(window).on('load', function(){
-    console.log('script');
+    
+    $('.my-flipster').flipster({
+        style: 'carousel',
+        start: 'center',
+        spacing: -0.5,
+        nav: false,
+        buttons: false,
+        loop: true,
+    });
+
+    $('.button-rockola').click(function(e){
+        e.preventDefault();
+        var href = $(this).attr('href');
+        
+        var myFlister = $('.my-flipster');
+        myFlister.flipster('jump', parseInt(href) );
+    });
 });

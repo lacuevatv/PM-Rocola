9056fc2fced634e681ddbddc08349e33d6aa6ec4
coco <coco@lacueva.tv>
2019-10-10 21:09:42 -0300

    ajuste el script para que haciendo clic en los botones de abajo se abra la lista igual

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -20,45 +20,32 @@ $(window).on('load', function(){
     $('.button-rockola').click(function(e){
         e.preventDefault();
         var href = $(this).attr('href');
-        
+        var idList = $(this).attr('data-list');
+
         var myFlister = $('.my-flipster');
         myFlister.flipster('jump', parseInt(href) );
 
         //agrega la clase active y elimina las restantes
         cleanActivesBtn(this);
+        
+        openTidal(idList);
     });
 
     $(document).on('click', '.cover', function(){
         event.preventDefault();
-        var wrapper = $('.tidal-wrapper');
-        $(wrapper).empty()
+        
 
         var idList = $(this).attr('href');
-        var html = '<div class="tidal-embed" data-type="p" data-id="'+idList+'"></div>';
         
-        $(wrapper).append($(html));
-
-        $('body').append( $('<script src="https://embed.tidal.com/tidal-embed.js"></script>') );
-
-       setTimeout(function(){
-        scrollToID('.tidal-wrapper');
-       }, 1000);
+        openTidal(idList);
         
     });
 
     /*
      *pone la lista de rock por defecto
      */
-    /*var html = '<div class="tidal-embed" data-type="p" data-id="94fe2b9b-096d-4b39-8129-d5b8e774e9b3"></div>';
-        
-    $('.tidal-wrapper').append($(html));
-
-    $('body').append( $('<script src="https://embed.tidal.com/tidal-embed.js"></script>') );
-
-    setTimeout(function(){
-    scrollToID('.tidal-wrapper');
-    }, 1000);
-    */
+    //openTidal('94fe2b9b-096d-4b39-8129-d5b8e774e9b3');
+    
 });
 
 //busca todos los botones activos y le quita la marca de activo y luego marca la que actualmente esta activa.
@@ -82,4 +69,19 @@ function scrollToID ( id ) {
     $('html, body').stop().animate({
         scrollTop: $(id).offset().top -90
     }, 'slow');
+}
+
+function openTidal(idList) {
+    var wrapper = $('.tidal-wrapper');
+    $(wrapper).empty()
+
+    var html = '<div class="tidal-embed" data-type="p" data-id="'+idList+'"></div>';
+    
+    $(wrapper).append($(html));
+
+    $('body').append( $('<script src="https://embed.tidal.com/tidal-embed.js"></script>') );
+
+    setTimeout(function(){
+    scrollToID('.tidal-wrapper');
+    }, 1000);
 }
